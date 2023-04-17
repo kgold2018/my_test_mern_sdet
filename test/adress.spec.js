a65ebdecc7e29a46c63d0db243a68969f2b4742c
Klavdia <kgold94@yahoo.com>
2023-04-17 16:07:53 -0700

    address test one

---
+++ test/adress.spec.js
@@ -0,0 +1,26 @@
+const baseUrl = "https://mern-ecommerce.sdet.school/api";
+const superagent = require('superagent');
+//response.body.token = undefined;
+describe("Test address endpoints" , ()=> {
+    let token;
+    beforeEach(async () => {
+        //login as Olsen Y
+        const reqBody = {
+            email: "Olsen.Y@gmail.com",
+            password: "Password1"
+        }
+        try {
+            const response = await superagent.post(baseUrl+"/auth/login").send(reqBody)
+            token = response.body.token
+        } catch (error) {
+            console.log("catch?")
+            console.error(error.message);
+        }
+    })
+
+    it("should run test", () =>{
+        console.log(token);
+
+    })
+
+})
